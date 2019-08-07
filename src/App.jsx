58b9b86a15fbc70cf5c569b7c9c0bb162e93c4e6
Johnny Bell <johnny@stackshare.io>
2019-08-07 14:36:08 -0700

    update a11y site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import Profile from './components/Profile';
+import Form from './components/Form';
+import Social from './components/Social';
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'arial'
+  }`;
+
+const Container = styled.div`
+  position: relative;
+  max-width: 1240px;
+  margin: 0 auto;
+`;
+
 const App = () => (
-  <div className="app">
-    <h2>My Amazing Site! 🤣</h2>
-    <label>Hi</label>
-    <input />
-    <button>Send</button>
-  </div>
+  <>
+    <GlobalStyle />
+    <Container>
+      <Profile />
+      <Social />
+      <Form />
+    </Container>
+  </>
 );
 
 export default App;
